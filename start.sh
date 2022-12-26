d38dc327f45ab7542900f813efb751e0fbede111
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2022-12-26 08:55:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /url-auto-delete-shortener-bot
 fi
-cd /Eva
+cd /url-auto-delete-shortener-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
